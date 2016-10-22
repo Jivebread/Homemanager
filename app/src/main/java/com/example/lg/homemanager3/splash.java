2ca2fad4b9aec4f4859712eda5331b939f0a4ff6
Jive Bread <jbnub12@gmai.com>
2016-10-22 03:05:12 -0500

    Splash added

---
+++ app/src/main/java/com/example/lg/homemanager3/splash.java
@@ -0,0 +1,61 @@
+package com.example.lg.homemanager3;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+/**
+ * Created by LG on 10/22/2016.
+ */
+
+public class splash extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        final ImageView imageViewWelcome = (ImageView) findViewById(R.id.splashimageid);
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeIn.setStartOffset(500);
+        fadeIn.setDuration(3000);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(3500);
+        fadeOut.setDuration(2000);
+
+        AnimationSet animation = new AnimationSet(false); //change to false
+        animation.addAnimation(fadeIn);
+        animation.addAnimation(fadeOut);
+        imageViewWelcome.setAnimation(animation);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                imageViewWelcome.setVisibility(View.INVISIBLE);
+                Intent myIntent = new Intent(splash.this, MainActivity.class);
+                splash.this.startActivity(myIntent);
+            }
+        }, 5500);
+    }
+
+
+}
+
